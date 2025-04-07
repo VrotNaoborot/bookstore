3373bfbd36e572a9e0a36c20e61b7e3cebc4bd7f
Panu <nktleznin@gmail.com>
2025-04-07 22:13:01 +0300

    add market

--- bookstore/core/views.py
+++ bookstore/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(requests):
     return render(requests, template_name="index.html")
+
+
+def market(request):
+    return render(request, template_name="market.html")
